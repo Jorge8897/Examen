cada0b9b30617b05ce2d05f111a015e2fa2a73b3
Jorge8897 <64932426+Jorge8897@users.noreply.github.com>
2020-06-02 22:39:09 -0600

    2
    2

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const hbs = require('hbs');
+const mongoose = require('mongoose');
+
+const PUERTO = process.env.PORT || 3000;
+
+let pintoresRouter = require('./routes/pintor');
+
+const app = express();
+app.set('view engine', 'hbs');
+hbs.registerPartial(__dirname + '/views/partials');
+app.use(express.static(__dirname + '/public'));
+
+app.use('/', pintoresRouter);
+
+mongoose.Promise = global.Promise;
+const cadena = 'mongodb+srv://JorgeR3T:jorger3t@gonzalezjorge-tfgrh.mongodb.net/test?retryWrites=true&w=majority'
+mongoose.connect(cadena,{useNewUrlParser: true, useUnifiedTopology: true})
+.then(()=>{
+    console.log('Conexion establecida');
+})
+.catch(err=> console.log(err));
+
+app.listen(PUERTO, ()=>{
+    console.log('Escuchando el puerto...')
+});
